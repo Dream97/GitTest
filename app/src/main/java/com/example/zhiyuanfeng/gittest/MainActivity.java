caf99cd605cfba05f20c8d5584dd02d2a3ee68b9
zhiyuan.feng <zhiyuanfeng@rastar>
2018-11-09 15:36:03 +0800

    添加新方法

--- app/src/main/java/com/example/zhiyuanfeng/gittest/MainActivity.java
+++ app/src/main/java/com/example/zhiyuanfeng/gittest/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.zhiyuanfeng.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void newMethod(){
+        Log.d(TAG, "newMethod: "+"这是新方法");
     }
 }
